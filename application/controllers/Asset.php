97c3d77952da54cb482662f0c858b396b06916fc
Henri Susanto <henrisusanto@Henris-MacBook-Pro.local>
2020-06-13 10:32:28 +0700

    autofill asset code

--- application/controllers/Asset.php
+++ application/controllers/Asset.php
@@ -10,7 +10,7 @@ class Asset extends MY_Controller {
 
 	function check ($barcode) {
 		$found = $this->Assets->findOne (array('AssetCode' => $barcode));
-		if (!$found) echo site_url ('Asset/create');
+		if (!$found) echo site_url ("Asset/create_asset/{$barcode}");
 		else echo site_url ("Asset/read/{$found['uuid']}");
 	}
 
@@ -20,4 +20,26 @@ class Asset extends MY_Controller {
 		$vars['js'] = array('quagga.min.js', 'scan.js');
 		$this->loadview('index', $vars);
 	}
+
+	function create_asset ($assetCode) {
+		$model= $this->model;
+		$vars = array();
+		$vars['page_name'] = 'form';
+		$vars['form']     = $this->$model->getForm();
+		foreach ($vars['form'] as $index => $field) {
+			if ('AssetCode' === $field['name']) {
+				$vars['form'][$index]['value'] = $assetCode;
+			}
+		}
+		$vars['subform'] = $this->$model->getFormChild();
+		$vars['uuid'] = '';
+		$vars['js'] = array(
+		  'moment.min.js',
+		  'bootstrap-datepicker.js',
+		  'daterangepicker.min.js',
+		  'select2.full.min.js',
+		  'form.js'
+		);
+		$this->loadview('index', $vars);
+	  }
 }
